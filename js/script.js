85e1736be427d34ae927c1e37d7a1b7a15028d95
Vitória Rodrigues Ferreira <vitoria.rf@outlook.com>
2025-10-01 16:07:19 -0300

    Feat: adicionar funções de pegar letras maiusculas e numeros

--- js/script.js
+++ js/script.js
@@ -8,3 +8,11 @@ const generatedPasswordElement = document.querySelector("#generated-password");
 const getLetterLowerCase = () => {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 };
+
+const getLetterUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+const getNumber = () => {
+    return Math.floor(Math.random() * 10).toString();
+}
